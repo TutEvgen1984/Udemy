3237644f010975d2f7f87d2e582abaa7d00679b5
TutEvgen1984 <35139330+TutEvgen1984@users.noreply.github.com>
2025-04-01 16:29:15 +0700

    01.04.2025

--- less7. Верстка по макету Figma/js/main.js
+++ less7. Верстка по макету Figma/js/main.js
@@ -1,10 +1,19 @@
+/* #region Инициализация sidenav */
 // https://materializecss.com/sidenav.html
 
 document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+    var sidenav = document.querySelectorAll('.sidenav');
+    var instances_sidenav = M.Sidenav.init(sidenav, {});
 });
 
 // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
 // var collapsibleElem = document.querySelector('.collapsible');
 // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+/* #endregion */
+
+/* #region Инициализация Modals */
+document.addEventListener('DOMContentLoaded', function () {
+    var modal = document.querySelectorAll('.modal');
+    var instances_modal = M.Modal.init(modal);
+});
+/* #endregion */
